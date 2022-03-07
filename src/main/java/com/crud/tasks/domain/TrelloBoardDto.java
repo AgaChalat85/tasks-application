5a17a077dbb51feb40ca942253a4f9503c5c1f30
Agnieszka Chalat <kontakt@akramarz.de>
2022-03-07 22:12:24 +0100

    22.3 Extending CreatedTrelloCard

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,8 +1,21 @@
 package com.crud.tasks.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TrelloBoardDto {
+
+    @JsonProperty("name")
     private String name;
+
+    @JsonProperty("id")
     private String id;
+
+    @JsonProperty("lists")
+    private List<TrelloListDto> lists;
 
     public TrelloBoardDto() {
 
@@ -19,5 +32,9 @@ public class TrelloBoardDto {
 
     public String getId() {
         return id;
+    }
+
+    public List<TrelloListDto> getLists() {
+        return lists;
     }
 }
