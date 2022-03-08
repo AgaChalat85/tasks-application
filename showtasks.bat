de4f0ba142f4ff907b0930ab5366958ee126243b
Agnieszka Chalat <kontakt@akramarz.de>
2022-03-08 22:28:08 +0100

    21.4 Creating showtasks.bat

---
+++ showtasks.bat
@@ -0,0 +1,20 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto browser
+echo.
+echo There were problems with running the runcrud.bat
+goto fail
+
+:browser
+start chrome "http://localhost:8080/crud/v1/tasks"
+if "%ERRORLEVEL%" == "0" goto end
+echo.
+echo There were problems with running the browser
+goto fail
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished
