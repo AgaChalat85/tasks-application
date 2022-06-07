d21d3d5c4c2c0c0f17cdd95158e084b4fcf25c6d
Agnieszka Chalat <kontakt@akramarz.de>
2022-06-07 11:02:28 +0200

    32.3 Thymeleaf- trello-card-mail2

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -30,7 +30,9 @@ public class SimpleEmailService {
     public void send(final Mail mail) {
         LOGGER.info("Starting email preparation...");
         try {
-            javaMailSender.send(createMimeMessage(mail));
+            javaMailSender.send(createSimpleMailMessage(mail));
+            //javaMailSender.send(createMimeMessage(mail));
+            //javaMailSender.send(createScheduledMessage(mail));
             LOGGER.info("Email has been sent.");
         } catch (MailException e) {
             LOGGER.error("Failed to process email sending: " + e.getMessage(), e);
@@ -43,6 +45,15 @@ public class SimpleEmailService {
             messageHelper.setTo(mail.getMailTo());
             messageHelper.setSubject(mail.getSubject());
             messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private MimeMessagePreparator createScheduledMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildScheduledMail(mail.getMessage()), true);
         };
     }
 
@@ -63,4 +74,5 @@ public class SimpleEmailService {
         ofNullable(mail.getToCC()).ifPresent(m -> mailMessage.setCc(mail.getToCC()));
         return mailMessage;
     }
+
 }
